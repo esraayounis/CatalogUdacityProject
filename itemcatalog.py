9bb3f39944dff1cd669cf52da68d3939cc44ea0c
Esraa Younis Mohamed <esraayounis61@gmail.com>
2019-01-01 04:58:57 +0200

    Update itemcatalog.py

--- itemcatalog.py
+++ itemcatalog.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from catalog_database import Base, Category, Item, User
 
-engine = create_engine('postgresql://catalog:grader@localhost/catalog')
+engine = create_engine('postgresql://catalog:graders@localhost/catalog')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
